7a92c0551a6bf9312d68205cf722ef5e5c6a0a7e
mdarsalan-c30 <105696006+mdarsalan-c30@users.noreply.github.com>
2025-05-07 01:02:14 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import AdminJobs from "./components/admin/AdminJobs";
 import PostJob from './components/admin/PostJob'
 import Applicants from './components/admin/Applicants'
 import ProtectedRoute from './components/admin/ProtectedRoute'
+import Webview from './components/Webview.jsx'
 
 
 const appRouter = createBrowserRouter([
